c5ef6443037b04ceb9da21d9a4bae786ea2a61ef
radhak52 <98499667+radhak52@users.noreply.github.com>
2023-01-11 22:20:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wqrujw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wqrujw)
---
+++ src/app/table/active.directive.ts
@@ -0,0 +1,6 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[my-active]',
+})
+export class MyActiveDirective {}
